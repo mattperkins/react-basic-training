10db1dc5b72cbf717e3b34c56f5db419b46f27d4
mattperkins <github@mattperkins.com>
2019-01-04 22:23:44 +0000

    add toggle check status functionality / pass props up through app components

--- src/components/MemoItem.js
+++ src/components/MemoItem.js
@@ -3,26 +3,23 @@ import PropTypes from 'prop-types'
 
 class MemoItem extends React.Component {
   getStatus = () => {
-    console.log('hello')
-
-  }
-  getStatus = () => {
     return {
       borderBottom: '1px dotted #ccc',
       padding: '30px',
+      cursor: 'pointer',
       textDecoration: this.props.memo.status ? 'line-through' : 'none',
       color: this.props.memo.status ? '#f33' : 'null'
     }
   }
 
-
-
   render() {
     console.log(this.props.memo)
     const { id, title } = this.props.memo
     return (
-      <div style={this.getStatus()}>
-        <p style={txtStyle}>{id}:{title}</p>
+      <div onClick={this.props.toggleStatus.bind(this, id)} style={this.getStatus()}>
+        <p style={txtStyle}>
+          {id}:{title}
+        </p>
       </div>
     )
   }
