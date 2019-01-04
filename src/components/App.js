a573ffc68e8af6efd527e5c2cf57e318f832371e
mattperkins <github@mattperkins.com>
2019-01-04 17:56:02 +0000

    add test data

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,25 @@ import './app.css'
 import Memo from './Memo'
 
 export default class App extends React.Component {
+  state = {
+    memos: [
+      {
+        id:1,
+        title: 'Book boardroom',
+        seen: false
+      },
+      {
+        id:2,
+        title: 'Order office supplies',
+        seen: true
+      },
+      {
+        id:3,
+        title: 'Xmas party 2019',
+        seen: false
+      },
+    ]
+  }
   render () {
     return (
       <Memo />
