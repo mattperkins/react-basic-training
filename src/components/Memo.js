98179b14e9dbf2f5ac2fb266666cb8c1643a2989
mattperkins <github@mattperkins.com>
2019-01-04 20:07:27 +0000

    scaffold component

--- src/components/Memo.js
+++ src/components/Memo.js
@@ -1,2 +1,16 @@
-import React from 'react'
-export default () => <p>Memo</p>
+import * as React from 'react'
+import MemoItem from './MemoItem'
+import PropTypes from 'prop-types'
+
+class Memos extends React.Component {
+  render () {
+    return (
+      <h1>{this.props.memos.map(memo => <MemoItem key={memo.id} memo={memo} />)}</h1>
+    )
+  }
+}
+
+Memos.propTypes = {
+  memos: PropTypes.array.isRequired
+}
+export default Memos
