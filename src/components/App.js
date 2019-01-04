214bd823791263bb458b44dda0258bd0db150ce1
mattperkins <github@mattperkins.com>
2019-01-04 17:34:15 +0000

    init

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import './app.css'
+import Memo from './Memo'
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <Memo />
+    )
+  }
+}
