70311ea47b6cb4af1cfebb87ccdbcfcab30bbb55
mattperkins <github@mattperkins.com>
2019-01-04 22:23:17 +0000

    add toggle check status functionality / pass props up through app components

--- src/components/App.js
+++ src/components/App.js
@@ -22,8 +22,18 @@ export default class App extends React.Component {
       }
     ]
   }
+  toggleStatus = (id) => {
+    this.setState({
+      memos: this.state.memos.map(memo => {
+        if (memo.id === id) {
+          memo.status = !memo.status
+        }
+        return memo
+      })
+    })
+  }
   render() {
     // console.log(this.state.memos)
-    return <Memo memos={this.state.memos} />
+    return <Memo memos={this.state.memos} toggleStatus={this.toggleStatus} />
   }
 }
