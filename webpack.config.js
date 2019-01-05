b30741b495553a140ba05318de0150253b7451e2
mattperkins <github@mattperkins.com>
2019-01-05 18:20:19 +0000

    format spacing

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
     ]
   },
   devServer: {
