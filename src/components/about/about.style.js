b22fbcbc10c575f76e3ebbdb17558fa311d66387
mattperkins <github@mattperkins.com>
2019-01-07 01:39:17 +0000

    refactor > isolate components/styles

---
+++ src/components/about/about.style.js
@@ -0,0 +1,14 @@
+function spacing(margin, padding) {
+  return {
+    margin,
+    padding
+  }
+}
+function txt(color, fontSize) {
+  return {
+    color,
+    fontSize
+  }
+}
+
+export { txt, spacing }
