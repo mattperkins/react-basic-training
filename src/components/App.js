d90bccc2e3c519af594ed57eb3f823610fc334b7
mattperkins <github@mattperkins.com>
2019-01-07 01:42:03 +0000

    add browser router and refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,100 +1,21 @@
 import * as React from 'react'
-import './app.css'
-import AddMemo from './AddMemo'
-import Button from './Button'
-import Memo from './Memo'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import MemoApp from './memo/MemoApp'
+import About from './about/About'
+require('./app.css')
 
 export default class App extends React.Component {
-  state = {
-    memos: [
-      {
-        id: 1,
-        title: 'Book boardroom',
-        status: false
-      },
-      {
-        id: 2,
-        title: 'Order office supplies',
-        status: true
-      },
-      {
-        id: 3,
-        title: 'Xmas party 2019',
-        status: false
-      }
-    ]
-  }
-
-  // Add Memo
-  addMemo = (title) => {
-    const createNewMemo = {
-      id: Math.random() * 10,
-      title,
-      status: false
-    }
-    this.setState({
-      memos: [...this.state.memos, createNewMemo]
-    })
-  }
-
-  toggleStatus = (id) => {
-    this.setState({
-      memos: this.state.memos.map(memo => {
-        if (memo.id === id) {
-          memo.status = !memo.status
-        }
-        return memo
-      })
-      // ,
-      // Delete Single Memo 
-      // memos: [...this.state.memos.filter(memo => memo.id !== id)]
-    })
-  }
-
-  deleteAll = () => {
-    this.setState({
-      memos: []
-    })
-  }
-
-  btnMsg = () => {
-    if (this.state.memos.length !== 0) {
-      return "Delete All"
-    } else {
-      return "..."
-    }
-  }
-
-  btnStyle = () => {
-    return {
-      width: '100%',
-      height: 100,
-      backgroundColor: '#f33',
-      fontSize: '3rem',
-      cursor: 'pointer',
-      position: 'fixed',
-      bottom: 0
-    }
-  }
-
   render() {
-
-    // console.log(this.state.memos)
     return (
-      <>
-        <AddMemo addMemo={this.addMemo} />
-        <Memo
-          memos={this.state.memos}
-          toggleStatus={this.toggleStatus} />
-        <div style={{ height: 200 }}></div>
-        <Button
-          btnStyle={this.btnStyle()}
-          deleteAll={this.deleteAll}
-          btnMsg={this.btnMsg()}
-        />
-
-      </>
-
+      <Router>
+        <>
+          <Route exact path="/" component={About} />
+          <Route path="/memo" render={props => (
+            <MemoApp />
+          )} />
+        </>
+      </Router>
     )
   }
 }
+
