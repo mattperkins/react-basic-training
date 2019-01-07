1330462213204fbedffdd7db08b61e5e6c0946e9
mattperkins <github@mattperkins.com>
2019-01-07 01:40:05 +0000

    refactor > isolate components/styles

---
+++ src/components/memo/Memo.js
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import MemoItem from './MemoItem'
+import PropTypes from 'prop-types'
+
+class Memos extends React.Component {
+
+  render() {
+    return (
+      <div>
+        {this.props.memos.map(memo =>
+          <MemoItem key={memo.id}
+            memo={memo}
+            toggleStatus={this.props.toggleStatus}
+          />)}
+      </div>
+    )
+  }
+}
+
+Memos.propTypes = {
+  memos: PropTypes.array.isRequired,
+  toggleStatus: PropTypes.func.isRequired,
+}
+export default Memos
